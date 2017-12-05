f44212c60921e9c96dca6e126dd3f5faf011f8c5
yingjiawei <yjw999wow@163.com>
2017-12-05 23:30:03 +0800

    接口与sealed类

--- app/src/main/java/com/apm29/yjw/kotlininaction/Main.kt
+++ app/src/main/java/com/apm29/yjw/kotlininaction/Main.kt
@@ -1,15 +1,21 @@
 /**
  * 内部类与嵌套类
  */
-class Main{
+
+/**
+ * sealed修饰的类默认为open，而且子类只能作为嵌套类或内部类集成
+ */
+sealed class Main{
     val field:String="field string"
     companion object {
         val TAG =this::class.simpleName
 
     }
     init {
-        val static=StaticClass()
-        val inner=InnerClass("new inner")
+        StaticClass()
+        InnerClass("new inner")
+        InnerClass()
+        InnerClass("key",99)
     }
 
     /**
@@ -41,5 +47,28 @@ class Main{
         init {
             println("init method no.2")
         }
+
+        /**
+         * 从构造方法 ，必须显式调用主构造或父类构造
+         */
+        constructor():this(""){
+            println(para)
+        }
+
+        constructor(key:String="keyTest", value:Int):this(""){
+            println(key+":"+value)
+        }
     }
+    interface Foo{
+        /**
+         * 接口中的方法可以有默认实现
+         */
+        fun foo():Unit= print("123")
+    }
+
+    /**
+     * 继承sealed类，接口没有构造，所以不用在继承时显式调用构造
+     */
+    inner class Child :Main(),Foo
+    class Child2:Main()
 }
